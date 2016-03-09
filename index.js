d46d1cd7a40ed4d9f3c27b8e01ffff2a4529a8d6
Peyman Mortazavi <pey.mortazavi@gmail.com>
2016-03-08 22:25:44 -0700

    added a web portal for looking at logs

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var dataCollector = require('./dataCollector.js');
 var MongoClient = require('mongodb').MongoClient;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+var currentStat = {daysLeft: 0, totalDays: 0, totalErrors: 0, totalRecords: 0};
 function handleProgress (info) {
-    console.log(info);
+    if (info.type == 'error') {
+        currentStat.totalErrors += 1;
+    } else if (info.type == 'stats') {
+        currentStat.totalDays = info.total;
+        currentStat.daysLeft = info.left;
+        currentStat.totalRecords = info.count;
+    }
+    if (info.type != 'stats') {
+        io.emit('log', info);
+    }
+    io.emit('stats', currentStat);
 }
 
 var url = 'mongodb://localhost:27017/git-test';
@@ -11,6 +25,20 @@ MongoClient.connect(url, function(err, db) {
         console.error(err);
         return;
     }
+    console.log('Connected to database, starting job now...');
 
     dataCollector.startJob(db.collection('repositories'), handleProgress);
+});
+
+// HTTP Connection
+app.get('/', function(req, res) {
+    res.sendfile('public/status.html');
+});
+
+io.on('connection', function(socket) {
+    socket.emit('stats', currentStat);
+});
+
+http.listen(80, function () {
+    console.log('Listening on *:80');
 });
